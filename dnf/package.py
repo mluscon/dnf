7239a893207126679626089729c2de483410f2e5
Michal Luscon <mluscon@redhat.com>
2016-02-28 21:55:10 +0100

    localPkg

--- dnf/package.py
+++ dnf/package.py
@@ -167,14 +167,17 @@ class Package(hawkey.Package):
         """
         if self.from_cmdline:
             return self.location
-        if self.baseurl:
-            path = os.path.join(self.baseurl, self.location)
-            if path.startswith("file://"):
-                path = path[7:]
-            return path
-        loc = self.location
-        if not self.repo.local:
-            loc = os.path.basename(loc)
+
+        if self.repo.local:
+            loc = self.location
+        else:
+            if self.baseurl:
+                path = os.path.join(self.baseurl, loc)
+                if path.startswith("file://"):
+                    path = path[7:]
+                return path
+            else:
+                loc = os.path.basename(loc)
         return os.path.join(self.repo.pkgdir, loc)
 
     # yum compatibility method
