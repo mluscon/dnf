99598a5256591c252b09f8e28f8491de6aeffddf
Michal Luscon <mluscon@redhat.com>
2016-04-10 23:15:53 +0200

    enforce-api: decorate Base class

--- dnf/cli/main.py
+++ dnf/cli/main.py
@@ -84,7 +84,7 @@ def _main(base, args):
     dnf.i18n.setup_stdout()
 
     # our core object for the cli
-    base.logging.presetup()
+    base._logging.presetup()
     cli = dnf.cli.cli.Cli(base)
 
     # do our cli parsing and config file setup
